90d82512f2811be708c1c47c6087ffcb310a2835
ChaYeDan <0920mzy@users.noreply.github.com>
2017-02-02 16:36:14 +0800

    Update Sorting.java
    modified array lenght

--- src/sorting/Sorting.java
+++ src/sorting/Sorting.java
@@ -6,6 +6,9 @@ public class Sorting
 {
 	public static void insert_sorting(int array[], int n)
 	{
+		int tmpArray[] = new int[n+1];
+		tmpArray[] = 0;
+		System.arraycopy(array, 0, tmpArray, 1, n);
 		int i = 0, j = 0;
 		for(i = 2; i <= n; i++)
 		{
@@ -21,11 +24,12 @@ public class Sorting
 				array[j+1] = array[0];
 			}
 		}
+		System.arraycopy(tmpArray, 1, array, 0, n);
 	}
 	
 	public static void main(String[] args)
 	{
-		int array[] = {0, 12, 34, 89, 90, 89, 1, 78, 56, 2, 2};
+		int array[] = {12, 34, 89, 90, 89, 1, 78, 56, 2, 2};
 		System.out.println("before sorting:"+ Arrays.toString(array));
 		insert_sorting(array, array.length-1);
 		System.out.println("after sorting:"+ Arrays.toString(array));
